83661c5add2574e3ebfa1ca2eb88a7326d65a06f
GustavoPisanello <99992149+GustavoPisanello@users.noreply.github.com>
2022-09-08 09:28:45 -0300

    08/09

---
+++ ExercicioVeiculos/ExercicioVeiculos/Carro.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioVeiculos
+{
+    public class Carro : Veiculo
+    {
+        private string numpassa;
+        private string carroce;
+        private string numport;
+        private string capaport;
+
+        public Carro(){
+            this.numpassa = null;
+            this.carroce = null;
+            this.numport = null;
+            this.capaport = null;
+        }
+        public Carro(string marca, string tipocombu, string poten, string peso, string placa, string preco,
+            string cambio, string auto, string datafab, string cor, string chassi, string numpassa, string carroce, string numport, string capaport)
+        {
+            this.Marca = marca;
+            this.TipoCombu = tipocombu;
+            this.Poten = poten;
+            this.Peso = peso;
+            this.Placa = placa;
+            this.Preco = preco;
+            this.Cambio = cambio;
+            this.Auto = auto;
+            this.Datafab = datafab;
+            this.Cor = cor;
+            this.Chassi = chassi;
+            this.NumPassa = numpassa;
+            this.Carroce = carroce;
+            this.NumPort = numport;
+            this.CapaPort = capaport;
+        }
+        public String NumPassa
+        {
+            get { return numpassa; }
+            set { numpassa = value; }
+        }
+        public String Carroce
+        {
+            get { return carroce; }
+            set { carroce = value; }
+        }
+        public String NumPort
+        {
+            get { return numport; }
+            set { numport = value; }
+        }
+        public string CapaPort
+        {
+            get { return capaport; }
+            set { capaport = value; }
+        }
+
+        public override string ImprimeDados()
+        {
+            String s = String.Empty;
+            s = String.Concat(this.carroce);
+            return s;
+        }
+    }
+}
--- ExercicioVeiculos/ExercicioVeiculos/Veiculo.cs
+++ ExercicioVeiculos/ExercicioVeiculos/Veiculo.cs
@@ -6,7 +6,106 @@ using System.Threading.Tasks;
 
 namespace ExercicioVeiculos
 {
-    internal class Veiculo
+    public abstract class Veiculo
     {
+        private string marca;
+        private string tipocombu;
+        private string poten;
+        private string peso;
+        private string placa;
+        private string preco;
+        private string cambio;
+        private string auto;
+        private string datafab;
+        private string cor;
+        private string chassi;
+
+        public Veiculo()
+        {
+            this.marca = null;
+            this.tipocombu = null;
+            this.poten = null;
+            this.peso = null;
+            this.placa = null;
+            this.preco = null;
+            this.cambio = null;
+            this.auto = null;
+            this.datafab = null;
+            this.cor = null;
+            this.chassi = null;
+        }
+        public Veiculo(string marca, string tipocombu, string poten, string peso, string placa, string preco, 
+            string cambio, string auto, string datafab, string cor, string chassi)
+        {
+            this.marca = marca;
+            this.tipocombu = tipocombu;
+            this.poten = poten;
+            this.peso = peso;
+            this.placa = placa;
+            this.preco = preco;
+            this.cambio = cambio;
+            this.auto = auto;
+            this.datafab = datafab;
+            this.cor = cor;
+            this.chassi = chassi;
+        }
+
+        public String Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+        public String TipoCombu
+        {
+            get { return tipocombu; }
+            set { tipocombu = value; }
+        }
+        public String Poten
+        {
+            get { return poten; }
+            set { poten = value; }
+        }
+        public String Peso
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+        public String Placa
+        {
+            get { return placa; }
+            set { placa = value; }
+        }
+        public String Preco
+        {
+            get { return preco; }
+            set { preco = value; }
+        }
+        public String Cambio
+        {
+            get { return cambio; }
+            set{ cambio = value;  }
+        }
+        public String Auto
+        {
+            get { return auto; }
+            set { auto = value; }
+        }
+        public String Datafab
+        {
+            get { return datafab; }
+            set { Datafab = value; }
+        }
+        public String Cor
+        {
+            get { return cor; }
+            set { cor = value; }
+        }
+        public String Chassi
+        {
+            get { return chassi; }
+            set { chassi = value; }
+        }
+
+        public abstract String ImprimeDados();
     }
 }
