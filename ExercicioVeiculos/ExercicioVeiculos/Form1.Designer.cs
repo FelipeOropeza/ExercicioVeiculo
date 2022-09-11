c82c2ed11a9a0443f4bd1c2feae2936a4c1fb9e7
FelipeOropeza <felipe.oropeza@etec.sp.gov.br>
2022-09-11 13:58:02 -0300

    11/09 Modificação do Design

--- ExercicioVeiculos/ExercicioVeiculos/Form1.Designer.cs
+++ ExercicioVeiculos/ExercicioVeiculos/Form1.Designer.cs
@@ -38,20 +38,21 @@
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.btnSair,
             this.cadastrosToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(364, 29);
+            this.menuStrip1.Size = new System.Drawing.Size(364, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // btnSair
             // 
-            this.btnSair.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSair.Name = "btnSair";
-            this.btnSair.Size = new System.Drawing.Size(49, 25);
+            this.btnSair.Size = new System.Drawing.Size(47, 24);
             this.btnSair.Text = "Sair";
             this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
             // 
@@ -60,22 +61,22 @@
             this.cadastrosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.btnCarro,
             this.btnCaminhao});
-            this.cadastrosToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cadastrosToolStripMenuItem.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cadastrosToolStripMenuItem.Name = "cadastrosToolStripMenuItem";
-            this.cadastrosToolStripMenuItem.Size = new System.Drawing.Size(91, 25);
+            this.cadastrosToolStripMenuItem.Size = new System.Drawing.Size(87, 24);
             this.cadastrosToolStripMenuItem.Text = "Cadastros";
             // 
             // btnCarro
             // 
             this.btnCarro.Name = "btnCarro";
-            this.btnCarro.Size = new System.Drawing.Size(151, 26);
+            this.btnCarro.Size = new System.Drawing.Size(180, 26);
             this.btnCarro.Text = "Carro";
             this.btnCarro.Click += new System.EventHandler(this.btnCarro_Click);
             // 
             // btnCaminhao
             // 
             this.btnCaminhao.Name = "btnCaminhao";
-            this.btnCaminhao.Size = new System.Drawing.Size(151, 26);
+            this.btnCaminhao.Size = new System.Drawing.Size(180, 26);
             this.btnCaminhao.Text = "Caminhão";
             this.btnCaminhao.Click += new System.EventHandler(this.btnCaminhao_Click);
             // 
@@ -83,6 +84,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(364, 130);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
